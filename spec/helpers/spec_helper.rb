ba0008e1ff526c45383c886768f2333f21bbbae2
Conrad Beach <conradbeach@gmail.com>
2017-02-09 13:08:37 -0700

    Update course spec. Add more mocks.

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -4,8 +4,10 @@ Minitest::Reporters.use!
 
 require "konbata"
 require_relative "../mocks/mock_course"
-require_relative "../mocks/mock_source_file"
+require_relative "../mocks/mock_cover_page_file"
 require_relative "../mocks/mock_glossary_file"
+require_relative "../mocks/mock_source_file"
+require_relative "../mocks/mock_unit_file"
 
 def fixture_path(fixture)
   File.absolute_path(File.join("spec", "fixtures", fixture))
