d5013d6a9f854df2a951ce547186e08ef7736a37
Conrad Beach <conradbeach@gmail.com>
2017-03-09 15:25:34 -0700

    Add canvas page for each PDF. Get course title from imsmanifest file.

--- lib/konbata/models/scorm_course.rb
+++ lib/konbata/models/scorm_course.rb
@@ -16,46 +16,95 @@
 require "zip"
 require "konbata/models/canvas_course"
 require "konbata/models/scorm_file"
+require "konbata/models/scorm_page"
 
 module Konbata
   class ScormCourse
     attr_reader :canvas_course
 
+    MANIFEST_FILENAME = "imsmanifest.xml".freeze
+
     def initialize(package_path)
       @package_path = package_path
-      # TODO: Consider using <title> node for course name.
-      @course_title = File.basename(package_path, ".zip")
-      @canvas_course = _create_canvas_course
+    end
+
+    def canvas_course
+      @canvas_course ||= begin
+        canvas_course = CanvasCourse.create(_course_title)
+        scorm_file = ScormFile.new(@package_path)
+
+        canvas_course.files << scorm_file.canvas_file
+        _pdf_files.each { |file| canvas_course.files << file.canvas_file }
+        _pdf_pages.each { |page| canvas_course.pages << page.canvas_page }
+
+        canvas_course
+      end
     end
 
     private
 
-    def _create_canvas_course
-      canvas_course = CanvasCourse.create(@course_title)
-      scorm_file = ScormFile.new(@package_path)
+    ##
+    # Finds the SCORM manifest in the zip package and returns it as a Nokogiri
+    # object.
+    ##
+    def _manifest
+      @scorm_manifest ||= begin
+        temp_dir = Dir.mktmpdir
+        manifest_path = File.join(temp_dir, MANIFEST_FILENAME)
 
-      canvas_course.files << scorm_file.canvas_file
-      _scorm_pdfs.each { |file| canvas_course.files << file.canvas_file }
+        Zip::File.new(@package_path).entries.detect do |entry|
+          entry.name == MANIFEST_FILENAME
+        end.extract(manifest_path)
 
-      canvas_course
+        File.open(manifest_path) { |file| Nokogiri::XML(file) }
+      end
     end
 
     ##
-    # Iterates through the SCORM package and detects any PDF files. A ScormFile
-    # object will be generated for each PDF found, and an array of ScormFile
-    # objects is returned.
+    # Reads the course title from the manifest file.
+    ##
+    def _course_title
+      @title ||= begin
+        default_org_id = _manifest.at(:organizations).attr(:default)
+
+        org = _manifest.at("organization[identifier='#{default_org_id}']")
+        org.at(:title).text
+      end
+    end
+
+    ##
+    # Iterates through the SCORM package and returns any PDF files.
     ##
     def _scorm_pdfs
-      temp_dir = Dir.mktmpdir
+      @pdfs ||= begin
+        temp_dir = Dir.mktmpdir
 
-      pdf_entries = Zip::File.new(@package_path).entries.select do |entry|
-        File.extname(entry.name) == ".pdf"
+        pdf_entries = Zip::File.new(@package_path).entries.select do |entry|
+          File.extname(entry.name) == ".pdf"
+        end
+
+        pdf_entries.map do |entry|
+          extract_to = File.join(temp_dir, File.basename(entry.name))
+          entry.extract(extract_to)
+
+          extract_to
+        end
       end
+    end
 
-      pdf_entries.map do |entry|
-        extract_to = File.join(temp_dir, File.basename(entry.name))
-        entry.extract(extract_to)
-        ScormFile.new(extract_to)
+    # Creates a ScormFile object for each element in _scorm_pdfs and returns
+    # them as an array.
+    def _pdf_files
+      _scorm_pdfs.map do |pdf|
+        ScormFile.new(pdf)
+      end
+    end
+
+    # Creates a ScormPage object for each element in _scorm_pdfs and returns
+    # them as an array.
+    def _pdf_pages
+      _scorm_pdfs.map do |pdf|
+        ScormPage.new(pdf)
       end
     end
   end
---
+++ lib/konbata/models/scorm_page.rb
@@ -0,0 +1,42 @@
+# Copyright (C) 2017  Atomic Jolt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require "canvas_cc"
+
+module Konbata
+  class ScormPage
+    attr_reader :canvas_page
+
+    def initialize(file_path)
+      @file_path = file_path
+      @canvas_page = _create_canvas_page
+    end
+
+    private
+
+    ##
+    # Creates and populates a canvas_cc page.
+    ##
+    def _create_canvas_page
+      page = CanvasCc::CanvasCC::Models::Page.new
+      page.identifier = Konbata.create_random_hex
+      page.workflow_state = "active"
+      page.page_name = File.basename(@file_path)
+      page.body = "This is a page."
+
+      page
+    end
+  end
+end
