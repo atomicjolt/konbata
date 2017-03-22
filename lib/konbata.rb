06598b299454f7b87190a704024b68227d1eb92a
Conrad Beach <conradbeach@gmail.com>
2017-03-22 15:24:04 -0600

    Update non-model source files.

--- lib/konbata.rb
+++ lib/konbata.rb
@@ -40,23 +40,6 @@ module Konbata
     "aj_" + SecureRandom.hex(32)
   end
 
-  def self.convert_docs
-    FileUtils.mkdir_p(OUTPUT_DIR)
-
-    docs = Dir.glob("#{INPUT_DIR}/**/*.{doc,docx}")
-
-    course_structures = generate_course_structures(docs)
-
-    course_structures.each_with_index do |(course_code, volumes), index|
-      Konbata::Reporter.start_course(course_code, index, course_structures.size)
-
-      course = Konbata::DocCourse.new(course_code, volumes)
-      create_imscc(course)
-
-      Konbata::Reporter.complete_course(course_code)
-    end
-  end
-
   def self.convert_scorm
     FileUtils.mkdir_p(OUTPUT_DIR)
 
@@ -78,36 +61,6 @@ module Konbata
       new(course.canvas_course).
       create(Dir.mktmpdir)
     FileUtils.cp(imscc, OUTPUT_DIR)
-  end
-
-  ##
-  # Returns a hash where each key is the course code and each value for the
-  # course code is a nested hash.
-  # The nested hashes have volume numbers for keys and an array of source file
-  # paths for that volume as the values.
-  # It also sorts the file path arrays so that module items are created and
-  # added in the right order.
-  ##
-  def self.generate_course_structures(entries)
-    course_structures = entries.each_with_object({}) do |entry, courses|
-      next unless File.file?(entry)
-
-      course = entry[/Course - ([^,]+)/i, 1]
-      volume = entry[/Volume (\d+)/i, 1]
-      file = File.absolute_path(entry)
-
-      courses[course] ||= Hash.new([])
-      courses[course][volume] |= [file]
-    end
-
-    course_structures.each do |course_code, volumes|
-      course_structures[course_code] = volumes.sort.to_h
-      volumes.each do |_volume, file_paths|
-        file_paths.sort! do |a, b|
-          File.basename(a).downcase <=> File.basename(b).downcase
-        end
-      end
-    end
   end
 
   def self.initialize_course(canvas_file_path)
--- lib/konbata/configuration.rb
+++ lib/konbata/configuration.rb
@@ -24,7 +24,6 @@ module Konbata
     attr_reader :scorm_launch_url
     attr_reader :scorm_shared_auth
     attr_reader :request_timeout
-    attr_reader :libre_office_path
     DEFAULT_TIMEOUT = 1_800 # 30 minutes
 
     def initialize
@@ -36,7 +35,6 @@ module Konbata
       @scorm_shared_auth = Configuration._config[:scorm_shared_auth]
       @request_timeout =
         Configuration._config[:request_timeout] || DEFAULT_TIMEOUT
-      @libre_office_path = Configuration._config[:libre_office_path]
     end
 
     def self._config
